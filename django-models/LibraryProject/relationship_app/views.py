ea6066b6e82306f9a6b2a048dfbd8f554751da4c
bamoos <kamfour1997@gmail.com>
2025-11-04 17:18:18 +0100

    Complete Tasks Week 10

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -82,7 +82,7 @@ def delete_book(request, book_id):
 # ====================== Library Detail ======================
 
 
-class LibraryDetailView(DetailView):
+class libraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
