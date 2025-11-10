588e265b42afd7cc931488c546fc8955318a7b86
bamoos <kamfour1997@gmail.com>
2025-11-10 17:53:44 +0100

    Complete tasks week 11 used CustomUser and groups & permissions geved to user and handle views and templates used

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,14 +1,21 @@
 from django import forms
 from .models import Book
 
+
+# ===================================================================================
+#  book form for create and update book
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author']
+
+# ===================================================================================
+# book search form
 class BookSearchForm(forms.Form):
     q = forms.CharField(max_length=200, required=False)
 
-
+# ===================================================================================
+# example form with basic fields
 class ExampleForm(forms.Form):
     title = forms.CharField(max_length=200, label='Book Title')
     author = forms.CharField(max_length=100, label='Author')
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,13 +3,21 @@ from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.decorators import login_required
 from .models import Book
 from .forms import BookForm
+from .forms import ExampleForm
+
+
+# =====================================================================================
+# get all book if has permission and user is logged in
 @login_required
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
+# =====================================================================================
 
 
+# =====================================================================================
+# create new book if has permission and user is logged in
 @login_required
 @permission_required('bookshelf.can_create', raise_exception=True)
 def book_create(request):
@@ -21,23 +29,11 @@ def book_create(request):
     else:
         form = BookForm()
     return render(request, 'bookshelf/form_example.html', {'form': form})
+# =====================================================================================
 
 
-@login_required
-@permission_required('bookshelf.can_edit', raise_exception=True)
-@login_required
-@permission_required('bookshelf.can_create', raise_exception=True)
-def book_create(request):
-    if request.method == 'POST':
-        form = BookForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('book_list')
-    else:
-        form = BookForm()
-    return render(request, 'bookshelf/form_example.html', {'form': form})
-
-
+# =====================================================================================
+# update book if has permission and user is logged in
 @login_required
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def book_edit(request, book_id):
@@ -51,12 +47,15 @@ def book_edit(request, book_id):
         form = BookForm(instance=book)
 
     return render(request, 'bookshelf/form_example.html', {'form': form, 'book': book})
+# =====================================================================================
 
 
+# =====================================================================================
+# delete book if has permission and user is logged in
 @login_required
 @permission_required('bookshelf.can_delete', raise_exception=True)
 def book_delete(request, book_id):
     book = Book.objects.get(id=book_id)
     book.delete()
     return redirect('book_list')
-
+# =====================================================================================
