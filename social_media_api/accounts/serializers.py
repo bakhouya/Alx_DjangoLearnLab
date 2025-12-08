a8771e410d3d74c58702fab68c721b3e5e36c035
bamoos <kamfour1997@gmail.com>
2025-12-08 14:15:04 +0100

    Fixed error add user to Model Token

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth import authenticate
 from django.contrib.auth.password_validation import validate_password
 from .models import CustomUser
+from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
@@ -30,6 +31,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, data):
         data.pop('password2')
         user = CustomUser.objects.create_user(**data)
+        Token.objects.create(user=user)
         return user
 
 
@@ -46,7 +48,6 @@ class LoginSerializer(serializers.Serializer):
                 raise serializers.ValidationError("Invalid credentials")
             else:
                 return {'user': user}
-
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
